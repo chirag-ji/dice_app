5485921274ca1422d826c7c478eb23c8e98997af
Chirag <apps.chiragji@outlook.in>
2021-07-18 19:01:41 +0530

    change the stateless widget to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,14 @@ void main() {
   ));
 }
 
-class DiceApp extends StatelessWidget {
+class DiceApp extends StatefulWidget {
+  const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
   @override
   Widget build(BuildContext context) {
     return Center(
